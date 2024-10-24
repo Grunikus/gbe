53a3bc02bce218ce1085315ac6bf4969e96f30fb
Grunikus <grunikus>
2024-10-24 22:05:14 +0200

    SUB and SBC

--- cpu.py
+++ cpu.py
@@ -1,6 +1,10 @@
 from memory import Memory
-from opcodes import ADD_A_B, ADD_A_C, ADD_A_D, ADD_A_E, ADD_A_H, ADD_A_L, ADD_A_HL, ADD_A_A
-from opcodes import ADC_A_B, ADC_A_C, ADC_A_D, ADC_A_E, ADC_A_H, ADC_A_L, ADC_A_HL, ADC_A_A
+from opcodes import (
+    ADD_A_B, ADD_A_C, ADD_A_D, ADD_A_E, ADD_A_H, ADD_A_L, ADD_A_HL, ADD_A_A,
+    ADC_A_B, ADC_A_C, ADC_A_D, ADC_A_E, ADC_A_H, ADC_A_L, ADC_A_HL, ADC_A_A,
+    SUB_A_B, SUB_A_C, SUB_A_D, SUB_A_E, SUB_A_H, SUB_A_L, SUB_A_HL, SUB_A_A,
+    SBC_A_B, SBC_A_C, SBC_A_D, SBC_A_E, SBC_A_H, SBC_A_L, SBC_A_HL, SBC_A_A,
+)
 
 FLAG_Z = 0b10000000  # Zero Flag
 FLAG_N = 0b01000000  # Subtract Flag
@@ -40,6 +44,26 @@ INSTRUCTION_MAP.update({
     ADC_A_HL:   lambda self: self.adc_hl(),
     ADC_A_A:    lambda self: self.adc_register(REGISTER_A),
 })
+INSTRUCTION_MAP.update({
+    SUB_A_B:    lambda self: self.sub_register(REGISTER_B),
+    SUB_A_C:    lambda self: self.sub_register(REGISTER_C),
+    SUB_A_D:    lambda self: self.sub_register(REGISTER_D),
+    SUB_A_E:    lambda self: self.sub_register(REGISTER_E),
+    SUB_A_H:    lambda self: self.sub_register(REGISTER_H),
+    SUB_A_L:    lambda self: self.sub_register(REGISTER_L),
+    SUB_A_HL:   lambda self: self.sub_hl(),
+    SUB_A_A:    lambda self: self.sub_register(REGISTER_A),
+})
+INSTRUCTION_MAP.update({
+    SBC_A_B:    lambda self: self.sbc_register(REGISTER_B),
+    SBC_A_C:    lambda self: self.sbc_register(REGISTER_C),
+    SBC_A_D:    lambda self: self.sbc_register(REGISTER_D),
+    SBC_A_E:    lambda self: self.sbc_register(REGISTER_E),
+    SBC_A_H:    lambda self: self.sbc_register(REGISTER_H),
+    SBC_A_L:    lambda self: self.sbc_register(REGISTER_L),
+    SBC_A_HL:   lambda self: self.sbc_hl(),
+    SBC_A_A:    lambda self: self.sbc_register(REGISTER_A),
+})
 
 class CPU:
     def __init__(self, memory: Memory):
@@ -58,7 +82,6 @@ class CPU:
         opcode = self.fetch_instruction()
         INSTRUCTION_MAP[opcode](self)
 
-    # Instruction implementations
     def _update_flags(self, result, subtraction=False):
         self.registers[REGISTER_F] = 0x00
         if result & 0xFF == 0:
@@ -73,6 +96,24 @@ class CPU:
         self.registers[REGISTER_A] = result & 0xFF
         self._update_flags(result)
 
+    def _adc_a(self, value):
+        carry = (self.registers[REGISTER_F] & FLAG_C) >> 4  # Extract the carry flag
+        result = self.registers[REGISTER_A] + value + carry
+        self.registers[REGISTER_A] = result & 0xFF
+        self._update_flags(result)
+
+    def _sub_a(self, value):
+        result = self.registers[REGISTER_A] - value
+        self.registers[REGISTER_A] = result & 0xFF
+        self._update_flags(result, subtraction=True)
+
+    def _sbc_a(self, value):
+        carry = (self.registers[REGISTER_F] & FLAG_C) >> 4  # Extract the carry flag
+        result = self.registers[REGISTER_A] - value - carry
+        self.registers[REGISTER_A] = result & 0xFF
+        self._update_flags(result, subtraction=True)
+
+    # Instruction implementations
     def add_register(self, register):
         self._add_a( self.registers[register] )
 
@@ -81,12 +122,6 @@ class CPU:
         memory_value = self.memory.read_byte(address)
         self._add_a(memory_value)
 
-    def _adc_a(self, value):
-        carry = (self.registers[REGISTER_F] & FLAG_C) >> 4  # Extract the carry flag
-        result = self.registers[REGISTER_A] + value + carry
-        self.registers[REGISTER_A] = result & 0xFF
-        self._update_flags(result)
-
     def adc_register(self, register):
         self._adc_a(self.registers[register])
 
@@ -94,3 +129,19 @@ class CPU:
         address = (self.registers[REGISTER_H] << 8) | self.registers[REGISTER_L]
         memory_value = self.memory.read_byte(address)
         self._adc_a(memory_value)
+
+    def sub_register(self, register):
+        self._sub_a(self.registers[register])
+
+    def sub_hl(self):
+        address = (self.registers[REGISTER_H] << 8) | self.registers[REGISTER_L]
+        memory_value = self.memory.read_byte(address)
+        self._sub_a(memory_value)
+
+    def sbc_register(self, register):
+        self._sbc_a(self.registers[register])
+
+    def sbc_hl(self):
+        address = (self.registers[REGISTER_H] << 8) | self.registers[REGISTER_L]
+        memory_value = self.memory.read_byte(address)
+        self._sbc_a(memory_value)
--- test_cpu.py
+++ test_cpu.py
@@ -106,5 +106,120 @@ class TestCPU(unittest.TestCase):
         self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_Z, 0x00)
         self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_N, 0x00)
 
+    def test_sub_register(self):
+        # Set registers
+        self.cpu.registers[REGISTER_B] = 0x02
+        self.cpu.registers[REGISTER_C] = 0x03
+        self.cpu.registers[REGISTER_D] = 0x04
+        self.cpu.registers[REGISTER_E] = 0x05
+        self.cpu.registers[REGISTER_H] = 0x06
+        self.cpu.registers[REGISTER_L] = 0x07
+
+        EXPECTED_RESULTS = {
+            opcodes.SUB_A_A: 0x00,  # A - A = 1 - 1
+            opcodes.SUB_A_B: 0xFF,  # A - B = 1 - 2
+            opcodes.SUB_A_C: 0xFE,  # A - C = 1 - 3
+            opcodes.SUB_A_D: 0xFD,  # A - D = 1 - 4
+            opcodes.SUB_A_E: 0xFC,  # A - E = 1 - 5
+            opcodes.SUB_A_H: 0xFB,  # A - H = 1 - 6
+            opcodes.SUB_A_L: 0xFA,  # A - L = 1 - 7
+        }
+
+        for opcode, expected_result in EXPECTED_RESULTS.items():
+            # Execute instruction
+            self.cpu.registers[REGISTER_A] = 0x01
+            self.memory.write_byte(self.cpu.pc, opcode)
+            self.cpu.step()
+            # Check result
+            self.assertEqual(self.cpu.registers[REGISTER_A], expected_result)
+            
+            # Check flags
+            if expected_result == 0x00:
+                # If the result is zero, Z flag should be set
+                self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_Z, FLAG_Z)
+            else:
+                # If the result is non-zero, Z flag should not be set
+                self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_Z, 0x00)
+            
+            # N flag should always be set for subtraction
+            self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_N, FLAG_N)
+            
+            # C flag should be set if the result caused an underflow
+            if expected_result > 0xFF:
+                self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_C, FLAG_C)
+            else:
+                self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_C, 0x00)
+
+    def test_sub_hl(self):
+        addr_high = 0x00
+        addr_low = 0x10
+        # Set registers and memory
+        self.cpu.registers[REGISTER_A] = 0x03
+        self.cpu.registers[REGISTER_H] = addr_high
+        self.cpu.registers[REGISTER_L] = addr_low
+
+        self.memory.write_byte((addr_high << 8) | addr_low, 0x02)  # Set memory at HL to 2
+
+        # Execute instruction
+        self.memory.write_byte(self.cpu.pc, opcodes.SUB_A_HL)
+        self.cpu.step()
+
+        # Check result
+        self.assertEqual(self.cpu.registers[REGISTER_A], 0x01)  # A = 3 - 2
+        self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_Z, 0x00)  # Z flag should be 0
+        self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_N, FLAG_N)  # N flag should be set
+
+    def test_sbc_register(self):
+        # Set registers
+        self.cpu.registers[REGISTER_B] = 0x02
+        self.cpu.registers[REGISTER_C] = 0x03
+        self.cpu.registers[REGISTER_D] = 0x04
+        self.cpu.registers[REGISTER_E] = 0x05
+        self.cpu.registers[REGISTER_H] = 0x06
+        self.cpu.registers[REGISTER_L] = 0x07
+
+        EXPECTED_RESULTS = {
+            opcodes.SBC_A_A: 0xFF,  # A = 1 - 1 - 1 (carry)
+            opcodes.SBC_A_B: 0xFE,  # A = 1 - 2 - 1 (carry)
+            opcodes.SBC_A_C: 0xFD,  # A = 1 - 3 - 1 (carry)
+            opcodes.SBC_A_D: 0xFC,  # A = 1 - 4 - 1 (carry)
+            opcodes.SBC_A_E: 0xFB,  # A = 1 - 5 - 1 (carry)
+            opcodes.SBC_A_H: 0xFA,  # A = 1 - 6 - 1 (carry)
+            opcodes.SBC_A_L: 0xF9,  # A = 1 - 7 - 1 (carry)
+        }
+
+        for opcode, expected_result in EXPECTED_RESULTS.items():
+            # Simulate carry flag set
+            self.cpu.registers[REGISTER_F] = FLAG_C  # Set carry flag
+            # Execute instruction
+            self.cpu.registers[REGISTER_A] = 0x01
+            self.memory.write_byte(self.cpu.pc, opcode)
+            self.cpu.step()
+            # Check result
+            self.assertEqual(self.cpu.registers[REGISTER_A], expected_result)
+            self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_Z, 0x00)  # Z flag should be 0
+            self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_N, FLAG_N)  # N flag should be set
+
+    def test_sbc_hl(self):
+        addr_high = 0x00
+        addr_low = 0x10
+        # Set registers and memory
+        self.cpu.registers[REGISTER_A] = 0x03
+        self.cpu.registers[REGISTER_H] = addr_high
+        self.cpu.registers[REGISTER_L] = addr_low
+
+        # Simulate carry flag set
+        self.cpu.registers[REGISTER_F] = FLAG_C  # Set carry flag
+        self.memory.write_byte((addr_high << 8) | addr_low, 0x02)  # Set memory at HL to 2
+
+        # Execute instruction
+        self.memory.write_byte(self.cpu.pc, opcodes.SBC_A_HL)
+        self.cpu.step()
+
+        # Check result
+        self.assertEqual(self.cpu.registers[REGISTER_A], 0x00)  # A = 3 - 2 - 1 (carry)
+        self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_Z, FLAG_Z)  # Z flag should be set
+        self.assertEqual(self.cpu.registers[REGISTER_F] & FLAG_N, FLAG_N)  # N flag should be set
+
 if __name__ == '__main__':
     unittest.main()
